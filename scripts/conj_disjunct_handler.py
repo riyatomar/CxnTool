aad881bf9194fa4075c76796ccbcec83e375ae43
riyatomar <riyatomar912@gmail.com>
2024-12-02 00:42:28 +0530

    Cxn Tool Code

--- scripts/conj_disjunct_handler.py
+++ scripts/conj_disjunct_handler.py
@@ -8,18 +8,34 @@ def handle_conj_disjunct(parser_output, new_entries, conj_count, disjunct_count,
             original_word = cc_item.get('original_word', '')
             op_count = 1
             matching_items = []
+            cxn_value1_index = None
+            cxn_value2_index = None
+            cxn_value1_component = None
+            cxn_value2_component = None
+
 
             for target_item in parser_output:
                 if int(target_item.get('head_index', -1)) == head_index and target_item.get('dependency_relation', '') == dep_rel and target_item.get('pos_tag') != 'CC':
                     cnx_index = len(parser_output) + len(new_entries) + 1
                     update_cnx_value(target_item, cnx_index, f'op{op_count}')
-                    cxn_value1 = target_item['cnx_value'][0].split(':')[0]
-                    cxn_value2 = target_item['cnx_value'][1]
-                    target_item['cnx_value'] = target_item['cnx_value'][0]
+                    
+                    if isinstance(target_item['cnx_index'], list) and len(target_item['cnx_index']) > 1:
+                        cxn_value1_index = target_item['cnx_index'][0]
+                        cxn_value1_component = target_item['cnx_component'][0]
+
+                        cxn_value2_index = target_item['cnx_index'][1]
+                        cxn_value2_component = target_item['cnx_component'][1]
+
+                        target_item['cnx_index'] = target_item['cnx_index'][0]
+                        target_item['cnx_component'] = target_item['cnx_component'][0]
+                    else:
+                        continue
 
                     for entry in new_entries:
-                        if entry.get('index') == int(cxn_value1):
-                            entry['cnx_value'] = cxn_value2
+                        if entry.get('index') == int(cxn_value1_index):
+                            entry['cnx_index'] = cxn_value2_index
+                            entry['cnx_component'] = cxn_value2_component
+
                     
                     matching_items.append(target_item)
                     op_count += 1
