92cea2bbdd91c2dd49d356140bdce1a142a6c41a
riyatomar <riyatomar912@gmail.com>
2024-12-14 23:12:49 +0530

    Updated Code

--- bulk_main.py
+++ bulk_main.py
@@ -12,37 +12,6 @@ from scripts.span_handler import handle_spans
 from scripts.conj_disjunct_handler import handle_conj_disjunct
 
 
-# def modify_json_data(json_data, ner_data):
-#     for response in json_data.get('response', []):
-#         parser_output = response.get('parser_output', [])
-
-#         # Reinitialize counters and temporary lists for each sentence
-#         span_count = nc_count = cp_count = disjunct_count = conj_count = spatial_count = meas_count = calendaric_count = 1
-#         ne_count = 0
-#         new_entries = []
-
-#         # Preprocess the parser_output
-#         for item in parser_output:
-#             # print(item)
-#             original_word = item.get('original_word', '')
-#             item['wx_word'] = devanagari_to_wx(original_word)
-
-#         # Process annotations and relations
-#         parser_output, ne_count, new_entries = handle_ner_annotations(ner_data, parser_output, ne_count, new_entries)
-#         # nc_count, ne_count = handle_mod_and_head(parser_output, new_entries, nc_count, ne_count) #
-#         cp_count = handle_pof_rvks_rbk(parser_output, new_entries, cp_count)
-#         meas_count = handle_measurement_units(parser_output, new_entries, meas_count, MEAS_UNITS)
-#         calendaric_count = handle_calendaric_units(parser_output, new_entries, calendaric_count, CALENDARIC_UNITS)
-#         spatial_count = handle_spatial_relations(parser_output, new_entries, spatial_count)
-#         span_count = handle_spans(parser_output, new_entries, span_count)
-#         conj_count, disjunct_count = handle_conj_disjunct(parser_output, new_entries, conj_count, disjunct_count, CONJ_LIST, DISJUNCT_LIST)
-
-#         # Append the new entries to the parser output
-#         parser_output.extend(new_entries)
-
-#     return json_data
-
-
 def modify_json_data(json_data, ner_data):
     for response in json_data.get('response', []):
         parser_output = response.get('parser_output', [])
@@ -68,7 +37,7 @@ def modify_json_data(json_data, ner_data):
 
         # Process annotations and relations for the current sentence
         parser_output, ne_count, new_entries = handle_ner_annotations(ner_annotations, parser_output, ne_count, new_entries)
-        nc_count, ne_count = handle_mod_and_head(parser_output, new_entries, nc_count, ne_count)
+        nc_count, ne_count = handle_mod_and_head(parser_output, new_entries, nc_count, ne_count) #
         cp_count = handle_pof_rvks_rbk(parser_output, new_entries, cp_count)
         meas_count = handle_measurement_units(parser_output, new_entries, meas_count, MEAS_UNITS)
         calendaric_count = handle_calendaric_units(parser_output, new_entries, calendaric_count, CALENDARIC_UNITS)
